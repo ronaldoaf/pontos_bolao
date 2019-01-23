30ccae241d6d4f52d3ce48ed76188b0f8aa2ce37
ronaldoaf <ronaldo.a.f@gmail.com>
2019-01-23 16:18:21 -0200

    Update pontos.js

--- pontos.js
+++ pontos.js
@@ -43,3 +43,13 @@ function retornoPlacar(placar){
 }
 
 $([  [0,0],[1,0],[0,1], [1,1],[2,0],[0,2],[2,1],[1,2],[3,0],[0,3],[3,1],[1,3]   ]).each(function(){ console.log([this[0]+'x'+this[1],retornoPlacar(this)])} );
+
+
+
+
+
+
+
+
+
+
